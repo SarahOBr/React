f84f059f4c66af8008d38726d3aec154da6d7427
Sarah O'Brien <sarah_obrien68@hotmail.com>
2022-03-07 19:47:10 +0000

    commit before counter

---
+++ src/components.jsx/Button.jsx
@@ -0,0 +1,7 @@
+export const Button = () => {
+    return <div>
+    <h1>"This is a title for Button"</h1>
+    <p>"This is a paragraph, practicing props and creating a button."</p>
+    <button>Props Button</button>
+</div>
+}
